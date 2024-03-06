9c5cea25747c83a6c8d166e0377fe136330862ec
haku4130 <andrejosipov19120@gmail.com>
2024-03-06 18:12:43 +0300

    Изменил способ отображения картинки на баннере, главного фото на маленьких экранах, скрипт по гпт, размер шрифта на баннере

--- public_html/static/app.js
+++ public_html/static/app.js
@@ -1,51 +1,65 @@
-document.getElementById('location').addEventListener('click', function () {
-  document.getElementById('map').scrollIntoView();
-});
+document
+  .getElementById('location')
+  .addEventListener('click', () =>
+    document.getElementById('map').scrollIntoView()
+  );
 
-document.getElementById('logo').addEventListener('click', function () {
-  window.scrollTo(0, 0);
-});
+document
+  .getElementById('logo')
+  .addEventListener('click', () => window.scrollTo(0, 0));
 
 let currentCardIndex = 0;
 const cards = document.querySelectorAll('.list-card');
 const hoverInterval = 3000;
+let hoverIntervalTimer = 0;
+
+let slideShowIndex = 1;
+let mainImgElement = document.getElementById('mainImage');
 
 function activateCard(index) {
-  // Удаляем активный класс со всех карт
   cards.forEach((card) => {
     card.classList.remove('active');
   });
 
-  // Добавляем активный класс к текущей карте
   const card = cards[index];
   if (card) {
     card.classList.add('active');
   }
 
-  // Обновляем индекс для следующей карты или сбрасываем его
   currentCardIndex = (index + 1) % cards.length;
 }
 
-// Запускаем циклическое переключение карт
-setInterval(() => {
-  activateCard(currentCardIndex);
-}, hoverInterval);
-
-let slideShowIndex = 1;
-let mainImgElement = document.getElementById('mainImage');
-
-function changeMainImage(index) {
-  // Первый шаг: делаем изображение невидимым
+function changeMainImage() {
   mainImgElement.style.opacity = 0;
 
-  // Второй шаг: после того как изображение стало невидимым, меняем источник и плавно показываем новое изображение
   setTimeout(() => {
-    mainImgElement.src = mainImageUrls[index];
-    mainImgElement.style.opacity = 1; // Делаем изображение видимым
-    slideShowIndex = (index + 1) % mainImageUrls.length;
-  }, 800); // Задержка соответствует времени перехода
+    mainImgElement.src = mainImageUrls[slideShowIndex];
+    mainImgElement.style.opacity = 1;
+    slideShowIndex = (slideShowIndex + 1) % mainImageUrls.length;
+  }, 800);
 }
 
-setInterval(() => {
-  changeMainImage(slideShowIndex);
-}, hoverInterval + 800); // Добавьте время анимации к интервалу, чтобы изображение успело измениться
+function changeCardOrImage(timestamp) {
+  if (hoverIntervalTimer <= 0) {
+    activateCard(currentCardIndex);
+    changeMainImage();
+    hoverIntervalTimer = hoverInterval;
+  } else {
+    hoverIntervalTimer -= timestamp - lastAnimationFrame;
+  }
+
+  lastAnimationFrame = timestamp;
+  requestAnimationFrame(changeCardOrImage);
+}
+
+lastAnimationFrame = performance.now();
+requestAnimationFrame(changeCardOrImage);
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'hidden') {
+    cancelAnimationFrame(lastAnimationFrame);
+  } else {
+    lastAnimationFrame = performance.now();
+    requestAnimationFrame(changeCardOrImage);
+  }
+});
